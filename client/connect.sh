6f02f64a00fc2b8d3d7493f0edf225ce7ef48de9
Tom Vijlbrief <tvijlbrief@gmail.com>
2016-09-25 18:10:25 +0200

    Fix connect.sh

--- client/connect.sh
+++ client/connect.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
+TUNHOST=root@my-docker-host.com
+
 echo Your public RSA key:
 cat /my_rsa_key.pub
 
+echo
+echo Store Host key on first run
+ssh -p 8443 $TUNHOST -i /my_rsa_key -o StrictHostKeyChecking=no date
+
+echo
+echo Check Remote Host:
+cat /root/.ssh/known_hosts
+echo
+
 while :
 do
+	echo -n "Connecting at: "
 	date
-	set -x
-	ssh -p 8443 root@your-docker-host.com -L 5432:some-postgres:5432 -i /my_rsa_key -N
+	ssh -p 8443 $TUNHOST -L *:5432:some-postgres:5432 -i /my_rsa_key -N
 	sleep 60
 done
